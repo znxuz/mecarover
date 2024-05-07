d9d19501ab257a60548c6103477deaa3321ca9da
Zijian Xu <znxuz@proton.me>
2024-05-07 08:59:57 +0200

    chore: removed unused microROS agent src files

--- agent/build/micro_ros_msgs/micro_ros_msgs__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/fabian/Schreibtisch/Workspace/ros2/agent/build/micro_ros_msgs/micro_ros_msgs__py
-# Build directory: /home/fabian/Schreibtisch/Workspace/ros2/agent/build/micro_ros_msgs/micro_ros_msgs__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
