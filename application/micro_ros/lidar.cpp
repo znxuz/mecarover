6a8fd8b1768772b09e6c75c21a571e92b32ad778
Zijian Xu <znxuz@proton.me>
2024-11-18 17:41:46 +0100

    chore: lidar DMA test comm

--- application/micro_ros/lidar.cpp
+++ application/micro_ros/lidar.cpp
@@ -16,6 +16,7 @@
 
 #include "rcl_ret_check.hpp"
 #include "stm32f7xx_hal_uart.h"
+#include "ulog.h"
 
 using namespace robot_params;
 
@@ -37,7 +38,7 @@ static std_msgs__msg__Bool enable_msg;
 
 static rcl_publisher_t data_sub;
 
-static uint8_t rx_buf[15];
+static uint8_t rx_buf[14];
 
 static void init_lidar_motor(void) {
   HAL_TIM_PWM_Start(&htim11, TIM_CHANNEL_1);
@@ -46,7 +47,7 @@ static void init_lidar_motor(void) {
 
 static void start(void) {
   __HAL_UART_CLEAR_IT(&huart2, UART_CLEAR_NEF | UART_CLEAR_OREF);
-  __HAL_TIM_SET_COMPARE(&htim11, TIM_CHANNEL_1, 5000);  // FIXME: ARR value
+  __HAL_TIM_SET_COMPARE(&htim11, TIM_CHANNEL_1, htim11.Instance->ARR);
   HAL_UART_Transmit_DMA(&huart2, START_CMD, sizeof(START_CMD));
 }
 
@@ -56,8 +57,7 @@ static void stop(void) {
 }
 
 static void enable_cb(const void* arg) {
-  // FIXME: do nothing now
-  // reinterpret_cast<const std_msgs__msg__Bool*>(arg)->data ? start() : stop();
+  reinterpret_cast<const std_msgs__msg__Bool*>(arg)->data ? start() : stop();
 }
 
 static void timer_cb(rcl_timer_t* timer, int64_t last_call_time) {
@@ -65,29 +65,33 @@ static void timer_cb(rcl_timer_t* timer, int64_t last_call_time) {
       HAL_UART_Transmit_DMA(&huart2, GET_HEALTH_CMD, sizeof(GET_HEALTH_CMD)));
   vTaskDelay(100);
 
-  // TODO idle line detection
+  ULOG_DEBUG(
+      "%02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x",
+      rx_buf[0], rx_buf[1], rx_buf[2], rx_buf[3], rx_buf[4], rx_buf[5],
+      rx_buf[6], rx_buf[7], rx_buf[8], rx_buf[9], rx_buf[10], rx_buf[11],
+      rx_buf[12], rx_buf[13]);
 }
 
 rclc_executor_t* lidar_init(rcl_node_t* node, rclc_support_t* support,
                             const rcl_allocator_t* allocator) {
-  rcl_ret_check(
+  rcl_ret_softcheck(
       rclc_executor_init(&exe, &support->context, N_EXEC_HANDLES, allocator));
 
-  rcl_ret_check(rclc_timer_init_default2(
+  rcl_ret_softcheck(rclc_timer_init_default2(
       &timer, support, RCL_MS_TO_NS(TIMER_TIMEOUT_MS), &timer_cb, true));
-  rcl_ret_check(rclc_executor_add_timer(&exe, &timer));
+  rcl_ret_softcheck(rclc_executor_add_timer(&exe, &timer));
 
-  rcl_ret_check(rclc_publisher_init_best_effort(
+  rcl_ret_softcheck(rclc_publisher_init_best_effort(
       &data_sub, node, ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, LaserScan),
       "lidar_data"));
 
-  rcl_ret_check(rclc_subscription_init_best_effort(
+  rcl_ret_softcheck(rclc_subscription_init_best_effort(
       &enable_sub, node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Bool),
       "lidar_enable"));
-  rcl_ret_check(rclc_executor_add_subscription(&exe, &enable_sub, &enable_msg,
-                                               &enable_cb, ON_NEW_DATA));
+  rcl_ret_softcheck(rclc_executor_add_subscription(
+      &exe, &enable_sub, &enable_msg, &enable_cb, ON_NEW_DATA));
 
-  // setup dma for receiving
+  // setup DMA for receiving
   rcl_ret_softcheck(HAL_UART_Receive_DMA(&huart2, rx_buf, sizeof(rx_buf)));
 
   init_lidar_motor();
--- application/micro_ros/micro_ros.cpp
+++ application/micro_ros/micro_ros.cpp
@@ -72,17 +72,17 @@ static void init() {
 void micro_ros(void* arg) {
   init();
 
-  auto* wheel_ctrl_exe = wheel_ctrl_init(&node, &support, &allocator);
-  auto* odometry_exe = odometry_init(&node, &support, &allocator);
-  auto* interpolation_exe = interpolation_init(&node, &support, &allocator);
+  // auto* wheel_ctrl_exe = wheel_ctrl_init(&node, &support, &allocator);
+  // auto* odometry_exe = odometry_init(&node, &support, &allocator);
+  // auto* interpolation_exe = interpolation_init(&node, &support, &allocator);
   auto* lidar_exe = lidar_init(&node, &support, &allocator);
 
   ULOG_INFO("micro-ROS: starting executors");
   for (;;) {
     // TODO: test with 1ms or even less
-    rclc_executor_spin_some(wheel_ctrl_exe, RCL_MS_TO_NS(5));
-    rclc_executor_spin_some(odometry_exe, RCL_MS_TO_NS(5));
-    rclc_executor_spin_some(interpolation_exe, RCL_MS_TO_NS(5));
+    // rclc_executor_spin_some(wheel_ctrl_exe, RCL_MS_TO_NS(5));
+    // rclc_executor_spin_some(odometry_exe, RCL_MS_TO_NS(5));
+    // rclc_executor_spin_some(interpolation_exe, RCL_MS_TO_NS(5));
     rclc_executor_spin_some(lidar_exe, RCL_MS_TO_NS(5));
   }
   // TODO: clean up functions from the executor modules, or even better: RAII
