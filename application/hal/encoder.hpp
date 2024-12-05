6d84d9edc0541a59e36ed7437a496b349112b3d0
Zijian Xu <znxuz@proton.me>
2024-12-05 12:10:33 +0100

    fix: timer interrupt callback runs on nullptr timer handle & before the encoder HAL is fully initialized

--- application/hal/encoder.hpp
+++ application/hal/encoder.hpp
@@ -12,12 +12,11 @@ class Encoder {
     this->offset = -(ARR_VALUE / 2);
 
     __HAL_TIM_SET_COUNTER(htim, ARR_VALUE / 2);
-    HAL_TIM_Encoder_Start(htim, TIM_CHANNEL_1);
-    HAL_TIM_Encoder_Start(htim, TIM_CHANNEL_2);
+    HAL_TIM_Encoder_Start(htim, TIM_CHANNEL_ALL);
     HAL_TIM_Base_Start_IT(htim);
 
     this->counter = __HAL_TIM_GET_COUNTER(htim);
-    this->htim = htim;
+    this->htim = htim;  // set `htim` to finalize the initialization
   }
 
   int32_t get_val() {
@@ -26,6 +25,13 @@ class Encoder {
   }
 
   void update_offset() {
+    if (!this->htim)
+      return;  // need to check because the interrupt callback
+               // `HAL_TIM_PeriodElapsedCallback` gets triggered immediately
+               // after `HAL_TIM_Base_Start_IT`, which calls this function while
+               // the whole thing not fully initialized yet and also the handle
+               // is still nullptr
+
     auto cnt = __HAL_TIM_GET_COUNTER(this->htim);
     if (cnt == this->counter) return;
     if (cnt < this->counter)
@@ -37,6 +43,6 @@ class Encoder {
  private:
   TIM_HandleTypeDef* htim = nullptr;
   /* 64-bit integer for not over or underflowing easily */
-  volatile int64_t offset;
-  int64_t counter;
+  volatile int64_t offset{};
+  int64_t counter{};
 };
