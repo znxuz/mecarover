7bf8c57c41ede4d924e4777d2cec7ea70bf7a181
Zijian Xu <znxuz@proton.me>
2025-01-17 16:50:12 +0100

    feat: odom

--- host/src/odom/src/odom.cpp
+++ host/src/odom/src/odom.cpp
@@ -3,8 +3,9 @@
 
 #include <chrono>
 #include <control_msgs/msg/mecanum_drive_controller_state.hpp>
-#include <geometry_msgs/msg/pose.hpp>
+#include <geometry_msgs/msg/pose2_d.hpp>
 #include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/float64.hpp>
 #include <std_msgs/msg/string.hpp>
 #include <utils/robot_params.hpp>
 #include <utils/transformations.hpp>
@@ -12,6 +13,7 @@
 using namespace std::chrono_literals;
 using namespace rclcpp;
 using namespace geometry_msgs::msg;
+using namespace transform;
 
 using drive_state = control_msgs::msg::MecanumDriveControllerState;
 using robot_params::VelRF;
@@ -19,59 +21,99 @@ using robot_params::VelWheel;
 
 using std::numbers::pi;
 
-static constexpr double max_pi(double theta) {
-  while (theta > pi) theta -= 2 * pi;
-  while (theta < -pi) theta += 2 * pi;
+namespace {
+double max_theta(double theta) {
+  while (theta > pi) theta -= (2 * pi);
+  while (theta < -pi) theta += (2 * pi);
   return theta;
 }
+
+Pose2D& operator+=(Pose2D& lhs, const Pose2D& rhs) {
+  lhs.x += rhs.x;
+  lhs.y += rhs.y;
+  lhs.theta += rhs.theta;
+  return lhs;
+}
+};  // namespace
 
 class Odom : public Node {
  public:
   Odom() : Node{"odom"} {
-    tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
+    epsilon_publisher_ =
+        this->create_publisher<std_msgs::msg::Float64>("topic", 10);
 
     encoder_data_subscription_ = this->create_subscription<drive_state>(
         "/mecarover/encoder_data", rclcpp::SensorDataQoS(),
         [this](drive_state::UniquePtr msg) { this->odometry(std::move(msg)); });
 
+    tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
+
     timer_ = this->create_wall_timer(timer_period_, [this]() {
-      RCLCPP_INFO(this->get_logger(), "gear ratio: %f",
-                  robot_params::GEAR_RATIO);
+      this->broadcast_odom_tf();
+      this->publish_epsilon();
     });
   }
 
  private:
   Subscription<drive_state>::SharedPtr encoder_data_subscription_;
+  Publisher<std_msgs::msg::Float64>::SharedPtr epsilon_publisher_;
   std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
   TimerBase::SharedPtr timer_;
-  Pose odom{};
+  Pose2D odom{};
+  double epsilon{};
   std::chrono::milliseconds timer_period_ = robot_params::POSE_CTRL_PERIOD_MS;
   double dt_ = std::chrono::duration<double>(timer_period_).count();
 
+  /*
+   * odometry: encoder delta gets fed directly into the inverted jacobian
+   * matrix without dividing the dt for the reason being:
+   * enc delta in rad / dt = vel -> forward_transform(vel) = vel_rf * dt =
+   * dpose
+   * => dt gets canceled out on both sides
+   */
   void odometry(drive_state::UniquePtr msg) {
-    /*
-     * odometry: encoder delta gets fed directly into the inverted jacobian
-     * matrix without dividing the dt for the reason being:
-     * enc delta in rad / dt = vel -> forward_transform(vel) = vel_rf * dt =
-     * dpose
-     * => dt gets canceled out on both sides
-     */
-    auto dpose_mtx = forward_transform(VelWheel{
-        msg->front_right_wheel_velocity, msg->front_left_wheel_velocity,
-        msg->back_left_wheel_velocity, msg->back_right_wheel_velocity});
+    auto mtx_to_pose2d = [](const VelWheel& dpose_mtx) -> Pose2D {
+      Pose2D dpose{};
+      dpose.set__x(dpose_mtx(0)).set__y(dpose_mtx(1)).set__theta(dpose_mtx(2));
 
-    tf2::Quaternion q{};
-    q.setRPY(0, 0, dpose_mtx(2));
+      return dpose;
+    };
 
-    geometry_msgs::msg::Pose dpose;
-    dpose.position.x = dpose_mtx(0);
-    dpose.position.y = dpose_mtx(1);
-    dpose.orientation.x = q.x();
-    dpose.orientation.y = q.y();
-    dpose.orientation.z = q.z();
-    dpose.orientation.w = q.w();
+    auto dpose_rf_mtx = forward_transform(
+        {msg->front_right_wheel_velocity, msg->front_left_wheel_velocity,
+         msg->back_left_wheel_velocity, msg->back_right_wheel_velocity});
+    dpose_rf_mtx(2) = max_theta(dpose_rf_mtx(2));
+    this->epsilon = dpose_rf_mtx(3);
 
-    // TODO dpose_mtx(3) for coupling error
+    odom += mtx_to_pose2d(
+        rotate_to_wframe(dpose_rf_mtx, odom.theta + dpose_rf_mtx(2) / 2));
+  }
+
+  void broadcast_odom_tf() const {
+    geometry_msgs::msg::TransformStamped t;
+
+    t.header.stamp = this->get_clock()->now();
+    t.header.frame_id = "odom";
+    t.child_frame_id = "base_link";
+
+    t.transform.translation.x = odom.x;
+    t.transform.translation.y = odom.y;
+    t.transform.translation.z = 0.0;
+
+    tf2::Quaternion q;
+    q.setRPY(0, 0, odom.theta);
+    t.transform.rotation.x = q.x();
+    t.transform.rotation.y = q.y();
+    t.transform.rotation.z = q.z();
+    t.transform.rotation.w = q.w();
+
+    tf_broadcaster_->sendTransform(t);
+  }
+
+  void publish_epsilon() const {
+    auto epsilon_msg = std_msgs::msg::Float64{};
+    epsilon_msg.data = this->epsilon;
+    epsilon_publisher_->publish(std::move(epsilon_msg));
   }
 };
 
