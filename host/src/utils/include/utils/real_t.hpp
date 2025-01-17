97dc2b9cc163ab9044fc9052c382c2a8fcfc4649
Zijian Xu <znxuz@proton.me>
2025-01-17 17:17:02 +0100

    chore: remove `real_t` definition, `pose_types.hpp`

--- host/src/utils/include/utils/real_t.hpp
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-using real_t = double;
--- host/src/utils/include/utils/robot_params.hpp
+++ host/src/utils/include/utils/robot_params.hpp
@@ -7,8 +7,6 @@
 #include <chrono>
 #include <numbers>
 
-#include "real_t.hpp"
-
 namespace robot_params {
 using namespace std::chrono;
 using namespace Eigen;
@@ -22,13 +20,13 @@ inline constexpr milliseconds LIDAR_PERIOD_MS = 200ms;
 inline constexpr uint8_t DOF = 4;
 inline constexpr uint8_t N_WHEEL = 4;
 
-inline constexpr real_t GEAR_RATIO = 64.0; // 1 wheel rot = 64 motor rot
-inline constexpr real_t INCREMENTS = 48.0; // 1 motor rot = 48 encoder incs
-inline constexpr real_t INC2RAD = 2.0 * pi / (INCREMENTS * GEAR_RATIO);
-inline constexpr real_t LENGTH = 325;
-inline constexpr real_t WIDTH = 300;
-inline constexpr real_t L_W_HALF = (LENGTH + WIDTH) / 2;
-inline constexpr real_t WHEEL_RADIUS = 50;
+inline constexpr double GEAR_RATIO = 64.0; // 1 wheel rot = 64 motor rot
+inline constexpr double INCREMENTS = 48.0; // 1 motor rot = 48 encoder incs
+inline constexpr double INC2RAD = 2.0 * pi / (INCREMENTS * GEAR_RATIO);
+inline constexpr double LENGTH = 325;
+inline constexpr double WIDTH = 300;
+inline constexpr double L_W_HALF = (LENGTH + WIDTH) / 2;
+inline constexpr double WHEEL_RADIUS = 50;
 /*
  * RPM given from the manufacturer: 120
  * experiment:
@@ -36,14 +34,14 @@ inline constexpr real_t WHEEL_RADIUS = 50;
  *  encoder increment delta on max PWM in 1 second: ~5375
  *  max rotation per minute = 5375 / 3072 * 60 ~= 105
  */
-inline constexpr real_t WHEEL_MAX_RPM = 105;
+inline constexpr double WHEEL_MAX_RPM = 105;
 
-inline constexpr real_t MAX_VELOCITY_WHEEL_ANGULAR =
+inline constexpr double MAX_VELOCITY_WHEEL_ANGULAR =
     WHEEL_MAX_RPM / 60 * (2 * pi);
-inline constexpr real_t MAX_VELOCITY_WHEEL_LINEAR =
+inline constexpr double MAX_VELOCITY_WHEEL_LINEAR =
     MAX_VELOCITY_WHEEL_ANGULAR * WHEEL_RADIUS;
 
-using VelWheel = Matrix<real_t, N_WHEEL, 1>;
-using VelRF = Matrix<real_t, DOF, 1>;
-using HomogenousTransformMatrix = Matrix<real_t, N_WHEEL, DOF>;
+using VelWheel = Matrix<double, N_WHEEL, 1>;
+using VelRF = Matrix<double, DOF, 1>;
+using HomogenousTransformMatrix = Matrix<double, N_WHEEL, DOF>;
 } // namespace robot_params
