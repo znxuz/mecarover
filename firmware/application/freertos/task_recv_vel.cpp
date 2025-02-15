c4e8b393eb134a686072a052844c7abc331d3dd1
Zijian Xu <znxuz@proton.me>
2025-02-15 16:22:07 +0100

    refactor: ISR receive velocity without making an extra copy

--- firmware/application/freertos/task_recv_vel.cpp
+++ firmware/application/freertos/task_recv_vel.cpp
@@ -7,13 +7,10 @@
 #include <ulog.h>
 #include <usart.h>
 
-#include <optional>
-
 #include "shared.hpp"
 #include "vel2d_frame.hpp"
 
 static uint8_t uart_rx_buf[VEL2D_FRAME_LEN];
-static std::optional<Vel2dFrame> frame;
 
 static TaskHandle_t task_handle;
 static size_t crc_err;
@@ -22,8 +19,6 @@ extern "C" {
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef* huart) {
   if (huart->Instance == huart3.Instance) {
     configASSERT(task_handle != NULL);
-
-    frame = *reinterpret_cast<const Vel2dFrame*>(uart_rx_buf);
 
     BaseType_t xHigherPriorityTaskWoken;
     vTaskNotifyGiveFromISR(task_handle, &xHigherPriorityTaskWoken);
@@ -40,18 +35,19 @@ static void task_impl(void*) {
     ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
 
     taskENTER_CRITICAL();
-    auto v = *frame;
+    auto frame = *reinterpret_cast<const Vel2dFrame*>(uart_rx_buf);
     taskEXIT_CRITICAL();
 
-    uint8_t* data = reinterpret_cast<uint8_t*>(&v.vel);
-    if (!v.compare(HAL_CRC_Calculate(&hcrc, reinterpret_cast<uint32_t*>(data),
-                                     sizeof(v.vel)))) {
+    auto* vel = reinterpret_cast<uint8_t*>(&frame.vel);
+    if (!frame.compare(HAL_CRC_Calculate(
+            &hcrc, reinterpret_cast<uint32_t*>(vel), sizeof(frame.vel)))) {
       ULOG_ERROR("crc mismatch!");
       ++crc_err;
       continue;
     }
 
-    xQueueSend(freertos::vel_sp_queue, &v.vel, NO_BLOCK);
+    ULOG_INFO("recv vel [%f %f %f]", frame.vel.x, frame.vel.y, frame.vel.z);
+    xQueueSend(freertos::vel_sp_queue, &frame.vel, NO_BLOCK);
   }
 }
 }
