27c8336a35f493d74874b86592884054d32db972
Zijian Xu <znxuz@proton.me>
2025-02-16 22:21:15 +0100

    feat: add static queue initialization

--- firmware/application/freertos/shared.cpp
+++ firmware/application/freertos/shared.cpp
@@ -22,6 +22,56 @@ QueueHandle_t vel_wheel_queue;
 
 static std::array<std::pair<QueueHandle_t*, std::function<QueueHandle_t(void)>>,
                   5>
+    static_queues{
+        std::make_pair(&vel_sp_queue,
+                       []() -> QueueHandle_t {
+                         constexpr size_t QUEUE_SIZE = 10;
+                         static vPose buf[QUEUE_SIZE];
+                         static StaticQueue_t static_queue;
+                         return xQueueCreateStatic(
+                             QUEUE_SIZE, sizeof(*buf),
+                             reinterpret_cast<uint8_t*>(buf), &static_queue);
+                       }),
+        std::make_pair(&enc_delta_odom_queue,
+                       []() {
+                         constexpr size_t QUEUE_SIZE = 1;
+                         static FourWheelData buf[QUEUE_SIZE];
+                         static StaticQueue_t static_queue;
+                         return xQueueCreateStatic(
+                             QUEUE_SIZE, sizeof(*buf),
+                             reinterpret_cast<uint8_t*>(buf), &static_queue);
+                       }),
+        std::make_pair(&enc_delta_wheel_ctrl_queue,
+                       []() {
+                         constexpr size_t QUEUE_SIZE = 10;
+                         static FourWheelData buf[QUEUE_SIZE];
+                         static StaticQueue_t static_queue;
+                         return xQueueCreateStatic(
+                             QUEUE_SIZE, sizeof(*buf),
+                             reinterpret_cast<uint8_t*>(buf), &static_queue);
+                       }),
+        std::make_pair(&odom_queue,
+                       []() {
+                         constexpr size_t QUEUE_SIZE = 10;
+                         static FourWheelData buf[QUEUE_SIZE];
+                         static StaticQueue_t static_queue;
+                         return xQueueCreateStatic(
+                             QUEUE_SIZE, sizeof(*buf),
+                             reinterpret_cast<uint8_t*>(buf), &static_queue);
+                       }),
+        std::make_pair(&vel_wheel_queue,
+                       []() {
+                         constexpr size_t QUEUE_SIZE = 10;
+                         static FourWheelData buf[QUEUE_SIZE];
+                         static StaticQueue_t static_queue;
+                         return xQueueCreateStatic(
+                             QUEUE_SIZE, sizeof(*buf),
+                             reinterpret_cast<uint8_t*>(buf), &static_queue);
+                       }),
+    };
+
+static std::array<std::pair<QueueHandle_t*, std::function<QueueHandle_t(void)>>,
+                  5>
     queues{
         std::make_pair(&vel_sp_queue,
                        []() { return xQueueCreate(10, sizeof(vPose)); }),
