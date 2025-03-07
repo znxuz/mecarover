e10aff63f6f3538d31741a9bd71c110feec3459b
Zijian Xu <znxuz@proton.me>
2025-03-07 16:46:56 +0100

    feat: enable instruction cache

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -76,6 +76,11 @@ int main(void)
 
   /* USER CODE END 1 */
 
+  /* Enable the CPU Cache */
+
+  /* Enable I-Cache---------------------------------------------------------*/
+  SCB_EnableICache();
+
   /* MCU Configuration--------------------------------------------------------*/
 
   /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
